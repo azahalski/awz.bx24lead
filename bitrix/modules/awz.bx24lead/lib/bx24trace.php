be81c0317dd4305d78430ef2e067b0c10602dbb7
Andrew Zahalski <andrew@zahalski.dev>
2025-06-14 12:46:57 +0300

    - добавление js для отправки трейса битрикс24 в фоне при загрузке страницы;

--- bitrix/modules/awz.bx24lead/lib/bx24trace.php
+++ bitrix/modules/awz.bx24lead/lib/bx24trace.php
@@ -35,4 +35,39 @@ class bx24Trace {
         return '';
     }
 
+    public static function getHitJs(string $type="", int $timeout=3000, bool $addDomReady = false): string
+    {
+        $context = Application::getInstance()->getContext();
+        $signer = new \Bitrix\Main\Security\Sign\Signer();
+        $signedParameters = $signer->sign(base64_encode(serialize([
+            'time'=>time()+60,
+            's_id'=>bitrix_sessid(),
+        ])));
+        if($type=='bx'){
+            \CJSCore::init(["ajax"]);
+            $html = '(function(){try{';
+            if($addDomReady) $html .= 'BX.ready(function(){';
+            $html .= 'setTimeout(function(){';
+            $html .= 'BX.ajax({';
+            $html .= 'url:"/bitrix/services/main/ajax.php?action=awz:bx24lead.api.trace.save",';
+            $html .= 'method:"POST",';
+            $html .= 'data:{"signed":"'.$signedParameters.'","TRACE":b24Tracker.guest.getTrace()}';
+            $html .= '});';
+            $html .= '},'.$timeout.');';
+            if($addDomReady) $html .= '});';
+            $html .= '}catch(e){console.log(e);}})();';
+        }else{
+            $html = '(function(){try{';
+            if($addDomReady) $html .= 'document.addEventListener("DOMContentLoaded", function(){';
+            $html .= 'setTimeout(function(){';
+            $html .= "let url = new URL('https://".$context->getServer()->getHttpHost()."/bitrix/services/main/ajax.php?action=awz:bx24lead.api.trace.save');";
+            $html .= "url.searchParams.set('signed', '".$signedParameters."');";
+            $html .= "url.searchParams.set('TRACE', b24Tracker.guest.getTrace());fetch(url);})";
+            $html .= "},'.$timeout.');";
+            if($addDomReady) $html .= "});";
+            $html .= '}catch(e){console.log(e);}})();';
+        }
+        return $html;
+    }
+
 }
