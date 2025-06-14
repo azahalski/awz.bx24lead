4683e1fd491a8697a661e51f4bacf27fcb593875
Andrew Zahalski <andrew@zahalski.dev>
2025-06-14 10:56:45 +0300

    gulp checkpoint

---
+++ update/1.1.3/lang/ru/admin/menu.php
@@ -0,0 +1,6 @@
+<?php
+$MESS['AWZ_BX24LEAD_MENU_NAME'] = 'AWZ: Интеграция с CRM';
+$MESS['AWZ_BX24LEAD_MENU_NAME_MENU1'] = 'Провайдеры данных';
+$MESS["AWZ_BX24LEAD_MENU_NAME_SETT"] = "Настройки модуля";
+$MESS["AWZ_BX24LEAD_MENU_NAME_SETT_1"] = "Общие настройки";
+$MESS["AWZ_BX24LEAD_MENU_NAME_SETT_2"] = "Права доступа";
